24f1027366778a9594de4817242f68b906106c7b
Kartik <kartik.tiwari@rakuten.com>
2025-05-26 09:18:40 +0900

    cses-1687 - fixed - Issue was using dfs functional so used Iterative approach PLUS instead of console logging inside loop, just add the answers to output array  and console log the output at once

--- Concept Questions/cses_company_queries_1687.js
+++ Concept Questions/cses_company_queries_1687.js
@@ -1,27 +1,52 @@
-"use strict";
+// AtCoder Boilerplate for Node.js 18.16.1
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
-function Main(input) {
-    // Split input into lines and set up a pointer
-    const lines = input.trim().split('\n');
-    let ptr = 0;
+let inputString = '';
+let currentLine = 0;
 
-    // Input helpers
-    const readLine = () => lines[ptr++] || "";
-    const readInt = () => parseInt(readLine(), 10);
-    const readInts = () => readLine().split(/\s+/).map(Number);
-    const readString = () => readLine().trim();
-    const readStrings = () => readLine().split(/\s+/);
-    const readBigInts = () => readLine().split(/\s+/).map(BigInt);
+process.stdin.on('data', (inputStdin) => {
+    inputString += inputStdin;
+});
 
-    // Example usage (replace with your logic):
+process.stdin.on('end', () => {
+    inputString = inputString.trim().split('\n');
+    main();
+});
 
-    // Read n and q (two integers on one line)
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function readInt() {
+    return parseInt(readLine());
+}
+
+function readInts() {
+    return readLine().split(' ').map(Number);
+}
+
+function readIntMatrix(n) {
+    let matrix = [];
+    for (let i = 0; i < n; i++) {
+        matrix.push(readInts());
+    }
+    return matrix;
+}
+
+function readStrings() {
+    return readLine().split(' ');
+}
+
+function readString() {
+    return readLine();
+}
+
+// Main function to handle the problem
+function main() {
+
     let [n, q] = readInts();
-
-    // Read an array of n-1 integers (next line)
     const arr = readInts();
-
-    // setAdjList(adjacency_list, arr);
 
     let parent = new Array(n+1).fill(-1).map(() => new Array(21).fill(0));
 
@@ -36,7 +61,7 @@ function Main(input) {
         }
     }
 
-    // dfs(1, 0, parent, adjacency_list);
+    let output = new Array();
 
     while(q--){
         let [x, k] = readInts();
@@ -53,11 +78,11 @@ function Main(input) {
             cnt++;
         }
 
-        if(elem === 0) console.log(-1);
-        else console.log(elem);
+        if(elem === 0) output.push(-1);
+        else output.push(elem);
 
-    }  
+    }
+
+    console.log(output.join('\n'));
+
 }
-///
-
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
